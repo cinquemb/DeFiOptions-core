729e6475d1df2d8e2e297b6d550bb2037b462432
cinquemb <1519017+cinquemb@users.noreply.github.com>
2022-07-23 23:13:38 -0400

    adding cdf, erf and exp functions to more math

--- contracts/utils/MoreMath.sol
+++ contracts/utils/MoreMath.sol
@@ -120,6 +120,109 @@ library MoreMath {
       }
     }
 
+    function cumulativeDistributionFunction(int256 x) internal pure returns (int256) {
+        // where p = 0.3275911,
+        // a1 = 0.254829592, a2 = −0.284496736, a3 = 1.421413741, a4 = −1.453152027, a5 = 1.061405429
+        // using 18 decimals
+        int256 p = 3275911e11;//0x53dd02a4f5ee2e46;
+        int256 one = 1e18;//ABDKMath64x64.fromUInt(1);
+        int256 two = 2e18;//ABDKMath64x64.fromUInt(2);
+        int256 a1 = 254829592e9;//0x413c831bb169f874;
+        int256 a2 = -284496736e9;//-0x48d4c730f051a5fe;
+        int256 a3 = 1421413741e9;//0x16a09e667f3bcc908;
+        int256 a4 = -1453152027e9;//-0x17401c57014c38f14;
+        int256 a5 = 1061405429e9;//0x10fb844255a12d72e;
+        int256 z = x.div(a3);
+        int256 t = one.div(one.add(p.mul(z.abs())));
+        int256 erf = getErrorFunction(z, t);
+        if (z < 0) {
+            erf = one.sub(erf);
+        }
+        int256 result = (one.div(two)).mul(one.add(erf));
+        return result;
+    }
+
+    function getErrorFunction(int256 z, int256 t) internal pure returns (int256) {
+        // where a1 = 0.254829592, a2 = −0.284496736, a3 = 1.421413741, a4 = −1.453152027, a5 = 1.061405429
+        // using 18 decimals
+        int256 step1;
+        {
+            int256 a3 = 1421413741e9;//0x16a09e667f3bcc908;
+            int256 a4 = -1453152027e9;//-0x17401c57014c38f14;
+            int256 a5 = 1061405429e9;//0x10fb844255a12d72e;
+            step1 = t.mul(a3.add(t.mul(a4.add(t.mul(a5)))));
+        }
+
+        int256 result;
+        {
+            int256 one = 1e18;//ABDKMath64x64.fromUInt(1);
+            int256 a1 = 254829592e9;//0x413c831bb169f874;
+            int256 a2 = -284496736e9;//-0x48d4c730f051a5fe;
+            int256 step2 = a1.add(t.mul(a2.add(step1)));
+            result = one.sub(
+                t.mul(
+                    step2.mul(
+                        int256(optimalExp(one.sub((z).pow(2))))
+                    )
+                )
+            );
+        }
+        return result;
+    }
+
+    /**
+      * @dev computes e ^ (x / FIXED_1) * FIXED_1
+      * input range: 0 <= x <= OPT_EXP_MAX_VAL - 1
+      * auto-generated via 'PrintFunctionOptimalExp.py'
+      * Detailed description:
+      * - Rewrite the input as a sum of binary exponents and a single residual r, as small as possible
+      * - The exponentiation of each binary exponent is given (pre-calculated)
+      * - The exponentiation of r is calculated via Taylor series for e^x, where x = r
+      * - The exponentiation of the input is calculated by multiplying the intermediate results above
+      * - For example: e^5.521692859 = e^(4 + 1 + 0.5 + 0.021692859) = e^4 * e^1 * e^0.5 * e^0.021692859
+      * - https://forum.openzeppelin.com/t/any-good-advanced-math-libraries-looking-for-square-root-ln-cumulative-distributions/2911
+    */
+    
+    function optimalExp(uint256 x) internal pure returns (uint256) {
+        uint256 constant FIXED_1 = 0x080000000000000000000000000000000;
+        uint256 res = 0;
+
+        uint256 y;
+        uint256 z;
+
+        z = y = x % 0x10000000000000000000000000000000; // get the input modulo 2^(-3)
+        z = z * y / FIXED_1; res += z * 0x10e1b3be415a0000; // add y^02 * (20! / 02!)
+        z = z * y / FIXED_1; res += z * 0x05a0913f6b1e0000; // add y^03 * (20! / 03!)
+        z = z * y / FIXED_1; res += z * 0x0168244fdac78000; // add y^04 * (20! / 04!)
+        z = z * y / FIXED_1; res += z * 0x004807432bc18000; // add y^05 * (20! / 05!)
+        z = z * y / FIXED_1; res += z * 0x000c0135dca04000; // add y^06 * (20! / 06!)
+        z = z * y / FIXED_1; res += z * 0x0001b707b1cdc000; // add y^07 * (20! / 07!)
+        z = z * y / FIXED_1; res += z * 0x000036e0f639b800; // add y^08 * (20! / 08!)
+        z = z * y / FIXED_1; res += z * 0x00000618fee9f800; // add y^09 * (20! / 09!)
+        z = z * y / FIXED_1; res += z * 0x0000009c197dcc00; // add y^10 * (20! / 10!)
+        z = z * y / FIXED_1; res += z * 0x0000000e30dce400; // add y^11 * (20! / 11!)
+        z = z * y / FIXED_1; res += z * 0x000000012ebd1300; // add y^12 * (20! / 12!)
+        z = z * y / FIXED_1; res += z * 0x0000000017499f00; // add y^13 * (20! / 13!)
+        z = z * y / FIXED_1; res += z * 0x0000000001a9d480; // add y^14 * (20! / 14!)
+        z = z * y / FIXED_1; res += z * 0x00000000001c6380; // add y^15 * (20! / 15!)
+        z = z * y / FIXED_1; res += z * 0x000000000001c638; // add y^16 * (20! / 16!)
+        z = z * y / FIXED_1; res += z * 0x0000000000001ab8; // add y^17 * (20! / 17!)
+        z = z * y / FIXED_1; res += z * 0x000000000000017c; // add y^18 * (20! / 18!)
+        z = z * y / FIXED_1; res += z * 0x0000000000000014; // add y^19 * (20! / 19!)
+        z = z * y / FIXED_1; res += z * 0x0000000000000001; // add y^20 * (20! / 20!)
+        res = res / 0x21c3677c82b40000 + y + FIXED_1; // divide by 20! and then add y^1 / 1! + y^0 / 0!
+
+        if ((x & 0x010000000000000000000000000000000) != 0) res = res * 0x1c3d6a24ed82218787d624d3e5eba95f9 / 0x18ebef9eac820ae8682b9793ac6d1e776; // multiply by e^2^(-3)
+        if ((x & 0x020000000000000000000000000000000) != 0) res = res * 0x18ebef9eac820ae8682b9793ac6d1e778 / 0x1368b2fc6f9609fe7aceb46aa619baed4; // multiply by e^2^(-2)
+        if ((x & 0x040000000000000000000000000000000) != 0) res = res * 0x1368b2fc6f9609fe7aceb46aa619baed5 / 0x0bc5ab1b16779be3575bd8f0520a9f21f; // multiply by e^2^(-1)
+        if ((x & 0x080000000000000000000000000000000) != 0) res = res * 0x0bc5ab1b16779be3575bd8f0520a9f21e / 0x0454aaa8efe072e7f6ddbab84b40a55c9; // multiply by e^2^(+0)
+        if ((x & 0x100000000000000000000000000000000) != 0) res = res * 0x0454aaa8efe072e7f6ddbab84b40a55c5 / 0x00960aadc109e7a3bf4578099615711ea; // multiply by e^2^(+1)
+        if ((x & 0x200000000000000000000000000000000) != 0) res = res * 0x00960aadc109e7a3bf4578099615711d7 / 0x0002bf84208204f5977f9a8cf01fdce3d; // multiply by e^2^(+2)
+        if ((x & 0x400000000000000000000000000000000) != 0) res = res * 0x0002bf84208204f5977f9a8cf01fdc307 / 0x0000003c6ab775dd0b95b4cbee7e65d11; // multiply by e^2^(+3)
+
+        return res;
+    }
+
     // rounds "v" considering a base "b"
     function round(uint v, uint b) internal pure returns (uint) {
 
