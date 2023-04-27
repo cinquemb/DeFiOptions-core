23e380f23335b660b41cd5e9447f5d4f5046beec
cinquemb <1519017+cinquemb@users.noreply.github.com>
2023-04-27 10:35:11 -0400

    add client order interface

--- contracts/interfaces/external/d8x/ID8xPerpetualsContractInterface.sol
+++ contracts/interfaces/external/d8x/ID8xPerpetualsContractInterface.sol
@@ -1,9 +1,38 @@
 pragma solidity >=0.6.0;
 pragma experimental ABIEncoderV2;
 
-import "../perpetual/interfaces/IClientOrder.sol";
 
 interface ID8xPerpetualsContractInterface {
+
+    //     iPerpetualId          global id for perpetual
+    //     traderAddr            address of trader
+    //     brokerSignature       signature of broker (or 0)
+    //     brokerFeeTbps         broker can set their own fee
+    //     fAmount               amount in base currency to be traded
+    //     fLimitPrice           limit price
+    //     fTriggerPrice         trigger price. Non-zero for stop orders.
+    //     iDeadline             deadline for price (seconds timestamp)
+    //     traderMgnTokenAddr    address of the compatible margin token the user likes to use,
+    //                           0 if same token as liquidity pool's margin token
+    //     flags                 trade flags
+    struct ClientOrder {
+        uint32 flags;
+        uint24 iPerpetualId;
+        uint16 brokerFeeTbps;
+        address traderAddr;
+        address brokerAddr;
+        address referrerAddr;
+        bytes brokerSignature;
+        int128 fAmount;
+        int128 fLimitPrice;
+        int128 fTriggerPrice;
+        int128 fLeverage; // 0 if deposit and trade separate
+        uint64 iDeadline;
+        uint64 createdTimestamp;
+        //uint64 submittedBlock <- will be set by LimitOrderBook
+        bytes32 parentChildDigest1;
+        bytes32 parentChildDigest2;
+    }
     
     /**
      * @notice  D8X Perpetual Data structure to store user margin information.
@@ -29,7 +58,7 @@ interface ID8xPerpetualsContractInterface {
         bytes16 positionId; // unique id for the position (for given trader, and perpetual). Current position, zero otherwise.
     }
     
-    function postOrder(IClientOrder.ClientOrder calldata _order, bytes calldata _signature)
+    function postOrder(ClientOrder calldata _order, bytes calldata _signature)
         external;
 
     function getMarginAccount(uint24 _perpetualId, address _traderAddress)
