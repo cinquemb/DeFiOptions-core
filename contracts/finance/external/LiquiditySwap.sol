0a0ac4812c0e871c88637ce362519b9e956b154b
cinquemb <1519017+cinquemb@users.noreply.github.com>
2023-11-03 01:26:08 -0400

    more comments

---
+++ contracts/finance/external/LiquiditySwap.sol
@@ -0,0 +1,70 @@
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "../../interfaces/ICreditToken.sol";
+import "../../interfaces/ICreditProvider.sol";
+import "../../interfaces/external/ISwapFlashLoan.sol";
+import "../../interfaces/external/ISwapUtils.sol";
+
+contract SwapFlashLoan {
+	...existing methods;
+
+	function mintForLiquidtySwap(uint256 underlyingDesiredTokenAmount) {}
+}
+
+
+contract LiquiditySwap is Ownable {
+	/*
+		- onwer of swapflashloan contracts need to
+ 				- mint enough lp tokens to cover enough requested stables
+ 					- redeem lp tokens for stables
+ 				- call swapTokenForCredit function on desired credit provider
+ 					- hold as long as desired, redeem debt for stables at interest
+ 					- deposit stables back to desired lp pool
+
+ 				- call swapForExchangeBalance(uint value) if desire for exchange balance for using options
+
+ 				- when redeeming (requestWithdraw) need to factor in lp:axial holder profit spit in contract
+ 					-25% sent to axial holders
+ 						- seperate contract that holds the funds before its sent back to sAxial holders?
+ 							- claiamable contract that will allow saxial stakers to claim their share relative to their sAxial ownership
+ 					-75% deposited back into liquidity pool
+
+ 		- assumptions
+ 			- apy on liquidy swap > base line aby on pool with no liquidity swap
+ 		- pros:
+ 			- more swap volume
+ 			- more revenue
+ 			- stakeholder in governance tokens for axial?
+ 			- can burn credit for exchange balance and use it to pursue options strategies
+ 		- cons:
+ 			- non zero increase in credit risk reliant upon demand for option buyers and traders using DOD
+	*/
+
+	constructor(address creditProvider, address creditToken, address tokenPool){}
+
+
+	function executeLiquiditySwap(uint256 amountUnderlying) ownlyGovernance external {
+		ISwapFlashLoan(tokenPool).mintForLiquidtySwap(amountUnderlying);
+		ISwapUtils.SwapUtils.Swap swapInfo = ISwapFlashLoan(tokenPool).swapStorage();
+		//TODO: make sure ISwapFlashLoan(tokenPool).calculateRemoveLiquidity == amountUnderlying after diluting existing lp holders
+
+		ISwapFlashLoan(tokenPool).removeLiquidity(uint256 amount, uint256[] calldata minAmounts,uint256 deadline);
+
+		for (uint i=0; i<swapInfo.pooledTokens.length;i++) {
+			ICreditProvider(creditProvider).swapTokenForCredit(address(this), address(swapInfo.pooledTokens[i]), swapInfo.pooledTokens[i].balanceOf(address(this)))
+		}
+	}
+
+	function executeCreditSwap(uint256 value) ownlyGovernance external{
+		//burning credit for exchange balance (no interest?)
+		ICreditToken(creditToken).swapForExchangeBalance(uint value);
+	}
+
+	function executeRequestWithdraw() ownlyGovernance external {
+		//burning credit for underlying tokens + interest
+		ICreditToken(creditToken).requestWithdraw();
+	}
+
+	function executeTransfer(address token, address to, uint256 amount) ownlyOwner external {}
+}
